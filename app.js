c67ea2acb047d43912d57adca8952d098a96cc70
Elijah <emadams92@gmail.com>
2021-01-07 12:52:04 -0500

    adding in angular app files

--- app.js
+++ app.js
@@ -1,39 +1,28 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
-
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
-
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
-
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const app = express();
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/ElijahMAdams')));
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/ElijahMAdams/index.html'));
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+/**
+ * Create HTTP server.
+ */
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+app.listen(port, () => console.log('Server running at http://127.0.0.1:' + port + '/') );
+
+
