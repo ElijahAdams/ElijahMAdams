b32f3165b9659bbe512ed3051d986befd843c5b6
Elijah <emadams92@gmail.com>
2021-01-25 21:14:22 -0500

    Responsive menu completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent, Observable, Subscription} from "rxjs";
+import {debounceTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Elijah Adams Website';
+export class AppComponent implements OnInit {
+  pages = [
+    {name: 'home', url: 'home'},
+    {name: 'about', url: 'about'},
+    {name: 'work', url: 'work'},
+    {name: 'contact', url: 'contact'},
+  ];
+  isSmallScreen = false;
+  showMobileMenu = false;
+  resizeObs: Observable<Event>;
+  resizeeSub: Subscription;
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.isSmallScreen = window.innerWidth < 600  ? true : false;
+    this.resizeObs = fromEvent(window, 'resize');
+    this.resizeeSub = this.resizeObs.pipe(debounceTime(25)).subscribe( evt => {
+      this.isSmallScreen = window.innerWidth < 600 ? true : false;
+    });
+  }
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
 }
