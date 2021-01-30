fc8c56819a82878082cf2bb2fbf317ac3886ddb3
Elijah <emadams92@gmail.com>
2021-01-29 20:14:06 -0500

    making the work example page responsive.

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit} from '@angular/core';
 import { data } from './work_examples.js'
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.scss']
 })
-export class WorkComponent implements OnInit {
-
+export class WorkComponent implements OnInit, AfterViewInit {
+  @Input() isSmallScreen;
+  descriptionLineHeight = 20;
   examples = data;
-  constructor() { }
+  selected = 0;
+  constructor(private totalBody: ElementRef) { }
 
   ngOnInit() {
   }
+  ngAfterViewInit() {
+    (this.totalBody.nativeElement as HTMLElement).
+      style.setProperty('--lineHeight', this.descriptionLineHeight + 'px');
+  }
 
-  hover(example) {
+  hover(example, i) {
+    const cardHeight = document.getElementById(`exampleCard${i}`).getBoundingClientRect().height;
+    // const titleHeight = document.getElementById(`title${i}`).getBoundingClientRect().height;
+    // const buttonHeight = document.getElementById(`buttonWrapper${i}`).getBoundingClientRect().height;
+    const remainderHeight = cardHeight - 70 - 45;
+    const allowDescriptionHeight = Math.floor(remainderHeight/ this.descriptionLineHeight) * this.descriptionLineHeight;
+
+    (this.totalBody.nativeElement as HTMLElement).
+    style.setProperty('--allowableDescriptionHeight',  allowDescriptionHeight + 'px');
     example.showText = true;
   }
 
