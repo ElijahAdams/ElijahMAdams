687d3e56245a1f6319b6456b1ec962c6fb0ed068
Elijah <emadams92@gmail.com>
2021-02-03 22:49:32 -0500

    adding content. for work experience

--- src/app/work/work_examples.js
+++ src/app/work/work_examples.js
@@ -1,13 +1,21 @@
 export const data = [
   {
     name: 'Retirement Readiness',
-    description: 'is a tool available to thousands of Vanguard institutional clients. It projects a users monthly ' +
-      'income at retirement based on their savings at Vanguard, estimated Social Security, ' +
-      'other benefits provided by their employer, and user entered information.',
+    description: 'is a tool available to Vanguard institutional clients. It projects a users monthly ' +
+      'income at retirement based on a few factors.',
     tech: 'TypeScript, Angular, Java, DB2',
-    challenge: 'The challenge was this...',
+    challenge: 'This tool\'s front-end was originally coded in AngularJS and the backend was done in java that ' +
+      'was tacked on to a monolith that started in the early 2000s. With AngularJS' +
+      ' being deprecated and Microservices becoming a best practice for the company it was time to migrate this app to newer technology.',
     showText: false,
-    overview: '',
+    overview: 'I worked on this project as a senior developer and served as the subject matter expert in Angular. ' +
+      'I trained the other developers on the team as well as implemented functionality throughout the technology stack ' +
+      'and changed the delivery pipeline. This tool combined a users financial data to determine their outlook for retirement. ' +
+      'A user would log into their account and be able to see a summary chart of what they could expect in monthly income during retirement. ' +
+      'Users would also have the ability to see how the calculation was made and make tweaks to the information present ' +
+      'to see how things will fair based profile and financial changes. Users could update, retirement age, social security collection age, ' +
+      'percent of salary desired in retirement, spouse data, retail investment information and so much more. This tool gave ' +
+      'comfort to those close to retirement and education for a game plan for those starting to save.',
     product: '',
     image: 'rr_1',
     imagesUrl: 'https://elijahmadamsresources.s3.amazonaws.com/vui_home.png',
@@ -15,13 +23,14 @@ export const data = [
   },
   {
     name: 'AngularJS Component Library',
-    description: 'Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare ' +
-      'vel eu leo. Sed posuere consectetur est at lobortis. Vivamus sagittis lacus vel augue ' +
-      'laoreet rutrum.',
-    tech: 'Javascript, Angularjs, Node.js, Webpack, Grunt',
-    challenge: 'The challenge was this...',
+    description: 'is a library that provides reusable (ada compliant) UI components for development teams to utilize in building front-end applications.',
+    tech: 'Javascript, AngularJS, Node.js, Webpack, Grunt',
+    challenge: 'This library aimed to reduce the time to market for applications across the company by providing UI components.',
     showText: false,
-    overview: 'Most notable the global header and footer on Vanguard\'s home page are a product of this library.',
+    overview: 'I worked on this project as a developer implementing components and updating the libraries documentation with examples. ' +
+      'Users would be able to view the documentation to learn how to use the different components and customize ' +
+      'them with different styles. The library had over 30 components that could be tested out in real time before using ' +
+      'them in a front-end projects. This library saved other developers a lot of time and help with brand consistency.',
     product: '',
     image: 'complib_1',
     imagesUrl: 'https://elijahmadamsresources.s3.amazonaws.com/vui_home.png',
@@ -29,12 +38,16 @@ export const data = [
   },
   {
     name: 'Acres',
-    description: 'Acres is an app that lets you find and support local black-owned businesses. ' +
-      'Currently servicing Charlotte, North Carolina.',
+    description: 'is a mobile app that lets users find and support local black-owned businesses. ' +
+      'Currently servicing the Charlotte, North Carolina area.',
     tech: 'Typescript, Ionic, Capacitor, Google Maps, Firebase',
-    challenge: 'The challenge was this...',
+    challenge: 'This app looks to help solve economic inequality by providing information on black owned businesses.',
     showText: false,
-    overview: '',
+    overview: 'I lead everything for this app. Users can download this app and it will provide information on ' +
+      'black-owned businesses in a list or map view. The information provided for each business is address, hours of operations,' +
+      ' directions, reviews and more. ' +
+      ' There are filters for the businesses to create the best search experience for users. ' +
+      'This app empowers people to help solve economic issues in the black community. ',
     product: 'https://play.google.com/store/apps/details?id=com.funktory.fortyAcres',
     image: 'acres',
     imagesUrl: 'https://elijahmadamsresources.s3.amazonaws.com/vui_home.png',
@@ -42,10 +55,12 @@ export const data = [
   },
   {
     name: 'TrueCount21',
-    description: 'This website shows how to count cards while playing Blackjack using the classic Hi-Lo system.',
+    description: 'is a pet project that shows users how to count cards while playing Blackjack using the classic Hi-Lo system.',
     tech: 'Typescript, Angular, Node.js, AWS',
-    challenge: 'This is a pet project for me.',
-    overview: '',
+    challenge: 'This website aims to teach users how to count cards to be able to take the casinos by storm when the covid pandemic ends.',
+    overview: 'I worked on this project for fun as the only developer. It has all the logic and game play of casino style Blackjack with ' +
+      'details for card counting. The website keeps track of all the necessary information to count card with the ability ' +
+      'for the user to toggle it on and off to practice this skill. With enough time a user can master the Hi-low system in this simulation to prepare for the real thing in casinos. ',
     showText: false,
     product: 'https://www.truecount21.com',
     image: 'truecount21',
